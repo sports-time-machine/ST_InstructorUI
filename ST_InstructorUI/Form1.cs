d052e3c99cba70bb524bb8dabe696f1372a1ef7e
eotel <a20621@g.ichinoseki.ac.jp>
2025-03-22 14:48:04 +0900

    refactor: update MySql.Data reference to use NuGet package
    - Changed MySql.Data reference to use NuGet package (Version 6.9.12.0) instead of local DLL.
    - Added `packages.config` to include NuGet package references.
    - Removed unused `Configuration` and `CultureInfo` imports in `Form1.cs`.
    
    This improves dependency management and aligns with best practices.

--- ST_InstructorUI/Form1.cs
+++ ST_InstructorUI/Form1.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Diagnostics;
 using System.Drawing;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
